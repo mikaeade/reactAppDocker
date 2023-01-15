d83812297ecfb9fa46ef36580b5f9dd5d933cf50
ME <eademika@gmail.com>
2023-01-15 19:41:23 +0200

    testi commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>MOROOO ooo</p>
+        <p>MOROOO AAAAAA</p>
         <a
           className="App-link"
           href="https://reactjs.org"
